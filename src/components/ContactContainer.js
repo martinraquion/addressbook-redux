5c9d7aaa10da80800e052abed768df8272822c29
Martin Raquion <raquionearl@gmail.com>
2020-07-08 23:53:00 +0800

    mapped through initial state

--- src/components/ContactContainer.js
+++ src/components/ContactContainer.js
@@ -1,23 +1,29 @@
 import React from "react";
 import { v1 as uuidv1 } from "uuid";
 import NewContact from "./NewContact";
+import { useSelector, useDispatch } from "react-redux";
 function ContactContainer() {
+  const contacts = useSelector((state) => state.contact);
   return (
     <div style={{ border: "1px solid black", padding: 10, width: 350 }}>
       <h2>CONTACT LIST</h2>
       <NewContact />
-      <div
-        style={{
-          border: "1px solid black",
-          display: "flex",
-          padding: 10,
-          flexDirection: "column",
-          alignItems: "flex-start",
-        }}
-      >
-        <span>Name: Martin Raquion</span>
-        <span>#: {uuidv1()} </span>
-      </div>
+      {contacts.map((contact) => (
+        <div
+          style={{
+            border: "1px solid black",
+            display: "flex",
+            padding: 10,
+            flexDirection: "column",
+            alignItems: "flex-start",
+            marginBottom: 4,
+          }}
+          key={contact.id}
+        >
+          <span>Name: {contact.name}</span>
+          <span>#: {contact.id} </span>
+        </div>
+      ))}
     </div>
   );
 }
